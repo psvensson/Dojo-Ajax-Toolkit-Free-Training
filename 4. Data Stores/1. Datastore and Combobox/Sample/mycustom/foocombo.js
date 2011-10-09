c2aa4718c565bad81ee3102c5fce063c338f5b6d
Peter Svensson <psvensson@gmail.com>
2011-10-09 14:48:05 +0200

    Added simple combobox example for datastore

---
+++ 4. Data Stores/1. Datastore and Combobox/Sample/mycustom/foocombo.js
@@ -0,0 +1,40 @@
+// Provide registers the class with dojo, so that it is not loaded twice
+dojo.provide("mycustom.foocombo");
+
+// All other classes that are used either as superclasses, as constructors or in other ways referred to must be 'required'
+dojo.require("dijit._Widget");
+dojo.require("dijit._Templated");
+dojo.require("dojo.data.ItemFileReadStore");
+dojo.require("dijit.form.ComboBox");
+
+// The declare statement actually creates the new class, named after the first argument. Superclasses are declared in the second argument,
+// and the class body (an object literal with all properties, functions as non-functions) is the third argument.
+dojo.declare("mycustom.foocombo", [ dijit._Widget, dijit._Templated ],
+{
+		// Here is the HTML template/snippet for the widget.
+		templateString:			dojo.cache("mycustom", "foocombo.html"),
+		// This enables us to define widgets inside our widget using 'dojoType'
+		widgetsInTemplate:	true,
+		data:			"",
+
+		// The postCreate function is called after the template is rendered, but before it is inserted into the DOM
+		postCreate:	function()
+		{
+			console.log("mycustom.foocombo postCreate called.");
+
+			var store = new dojo.data.ItemFileReadStore({data: this.data});
+
+			var args = 
+			{
+				name:"LeBox",
+				title: "title attribute used for label",
+				autoComplete:false,
+				store: store,
+				searchAttr:"name"
+			};
+
+			this.thecombowidget = new dijit.form.ComboBox(args, this.thecomboboxnode); 
+		}
+
+		
+});
